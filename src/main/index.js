0fef8f18673ba8d7d7fd481924b9ff58c4a345d6
anandita-3217 <dakshugarimella@gmail.com>
2026-07-21 16:07:08 +0530

    db works

--- src/main/index.js
+++ src/main/index.js
@@ -117,7 +117,6 @@ function deserializeTechnique(row) {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  console.log('userData path:', app.getPath('userData'))
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
 

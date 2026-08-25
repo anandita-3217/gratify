9d789b4bdab6623da79579c8c369df9a8f93c2a3
anandita-3217 <dakshugarimella@gmail.com>
2026-08-25 07:58:35 +0530

    app lvl ssearch

--- src/renderer/src/pages/Dashboard/index.jsx
+++ src/renderer/src/pages/Dashboard/index.jsx
@@ -481,7 +481,7 @@ export default function Dashboard({ onNavigate }) {
           </Paper>
         ))}
       </Group>
-      <Stack gap={4}>
+      <Stack gap={'md'}>
         <TextInput label="Search Sth" />
       </Stack>
       <Stack gap="md">

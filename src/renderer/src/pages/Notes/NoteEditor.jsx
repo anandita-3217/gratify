ae78ca6a04388d7e10a4c52f3aa87ee782f0087e
anandita-3217 <dakshugarimella@gmail.com>
2026-09-16 10:40:07 +0530

    mods

--- src/renderer/src/pages/Notes/NoteEditor.jsx
+++ src/renderer/src/pages/Notes/NoteEditor.jsx
@@ -242,6 +242,7 @@ function convertToBulletList(editor) {
   editor.commands.setContent(html)
 }
 // TODO: Check refresh thingy
+
 export function extractChecklistItems(html) {
   const parser = new DOMParser()
   const doc = parser.parseFromString(html, 'text/html')
